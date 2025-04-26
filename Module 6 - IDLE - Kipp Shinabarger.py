1cb4fbe5345cb7ba9bdd86567ba396cc3d2a5371
KippWade <43067502+KippWade@users.noreply.github.com>
2025-04-26 17:06:49 -0400

    Add files via upload

---
+++ Module 6 - IDLE - Kipp Shinabarger.py
@@ -0,0 +1,8 @@
+# Variable to hold the word "cats"
+c = "cats"
+# Variable to hold the world "dogs"
+d = "dogs"
+# Variable to hold the contatonnated sentance to be output
+s = "It is raining " + c + " and " + d + "."
+# Output the sentence
+print(s)
